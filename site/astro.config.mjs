2aec1d8b349df1d56e80776a4c90f0b37fb9d55f
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2025-06-23 07:04:07 -0400

    Automatically add heading IDs to all pages (even non-markdown)

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+import { load } from "cheerio";
+import fg from "fast-glob";
+import GithubSlugger from "github-slugger";
+
+import { readFile, writeFile } from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +21,31 @@ export default defineConfig({
     icon({
       iconDir: "src/assets/icons",
     }),
+    {
+      /**
+       * Ensures all headings have IDs
+       * (even *.astro pages, which can't be done via `markdown` config)
+       */
+      name: "heading-ids",
+      hooks: {
+        "astro:build:done": async ({ dir }) => {
+          const distPath = fileURLToPath(dir);
+          const htmlPaths = await fg.glob("**/*.html", { cwd: distPath });
+          const start = Date.now();
+          const slugger = new GithubSlugger();
+          for (const path of htmlPaths) {
+            slugger.reset();
+            const filePath = join(distPath, path);
+            const $ = load(await readFile(filePath));
+
+            $("h1, h2, h3, h4, h5, h6").each((_, el) => {
+              if (!el.attribs.id) el.attribs.id = slugger.slug($(el).text());
+            });
+            await writeFile(filePath, $.html());
+          }
+          console.log(`Heading ID plugin completed in ${Date.now() - start}ms`);
+        },
+      },
+    },
   ]
 });
