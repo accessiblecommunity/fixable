6fb5014880dc15aadd52a0dc98c433e2d978bfc2
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2026-03-10 18:23:22 -0400

    Remove support for multiple break descriptions in one entry

--- site/src/content.config.ts
+++ site/src/content.config.ts
@@ -184,7 +184,7 @@ export const collections = {
     // in order for typings for references and transforms to work
     schema: z
       .object({
-        description: singleOrArray(z.string()).transform(transformToArray),
+        description: z.string(),
         discussionItems: z.array(z.string()).nonempty().optional(),
         href: z.string().regex(/^(\/|#|$)/),
         location: reference("breakSections"),
